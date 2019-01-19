cc8ce25fab794bcfeec8d218d6b91e55f9a46fdf
J. Famous <joshua.famous@gmail.com>
2019-01-19 17:52:46 -0500

    Title and Timegun

---
+++ Assets/Scripts/cs1_controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cs1_controller : MonoBehaviour {
+
+	private bool hasGun;
+
+	// Use this for initialization
+	void Start () {
+		hasGun = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
