201248ae4f94254d913199d16d12a97f739b96f4
J. Famous <joshua.famous@gmail.com>
2019-01-19 18:18:34 -0500

    Switch canvas to classroom

--- Assets/Scripts/cs1_controller.cs
+++ Assets/Scripts/cs1_controller.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class cs1_controller : MonoBehaviour {
 
 	private bool hasGun;
+	public GameObject player;
+	public GameObject canvas;
+	public GameObject starterCamera;
+	public GameObject playerCamera;
 
 	// Use this for initialization
 	void Start () {
 		hasGun = false;
+		player.SetActive (false);
+		canvas.SetActive (true);
+		starterCamera.SetActive (true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.E)){
+			Debug.Log ("Campos" + playerCamera.transform.position.x + " y : " + playerCamera.transform.position.y + " z : " + playerCamera.transform.position.z);
+		}
 	}
+
+	public void BeginFade(){
+
+	}
+
+	public void StartGame(){
+		player.SetActive (true);
+		canvas.SetActive (false);
+		starterCamera.SetActive (false);
+	}
+		
 }
