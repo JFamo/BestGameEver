8876d8b587461863ee467f8b12eea7d5e2a4da00
J. Famous <joshua.famous@gmail.com>
2019-01-29 02:16:51 -0500

    Greek Intro Effect

--- Assets/Scripts/cs2_controller.cs
+++ Assets/Scripts/cs2_controller.cs
@@ -1,16 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class cs2_controller : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject player;
+	private FirstPersonController characterControllerScript;
+	public GameObject greekIntroDialogue;
+	public GameObject sprintDialogue;
+	public GameObject eyeTop;
+	public GameObject eyeBottom;
+
+	private bool hasShownSprintMsg;
+
 	void Start () {
-		
+		characterControllerScript = player.GetComponent<FirstPersonController> ();
+		characterControllerScript.canLookAround = false;
+		characterControllerScript.canWalk = false;
+
+		hasShownSprintMsg = false;
+
+		StartCoroutine(StartGame (2.30f));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update(){
+		if (!hasShownSprintMsg) {
+			if (player.transform.position.x > 60) {
+				if(greekIntroDialogue.activeInHierarchy){
+					StartCoroutine (HideMessage (greekIntroDialogue, 0.1f));
+				}
+				sprintDialogue.SetActive (true);
+				hasShownSprintMsg = true;
+				StartCoroutine (HideMessage (sprintDialogue, 4.0f));
+			}
+		}
+	}
+
+	//from the time the level loads to the time the player is given control
+	IEnumerator StartGame(float delay){
+		yield return new WaitForSeconds (delay);
+		characterControllerScript.canLookAround = true;
+		characterControllerScript.canWalk = true;
+		eyeTop.SetActive (false);
+		eyeBottom.SetActive (false);
+		greekIntroDialogue.SetActive (true);
+		StartCoroutine (HideMessage (greekIntroDialogue, 5.0f));
+	}
+
+	IEnumerator HideMessage(GameObject go, float delay){
+		yield return new WaitForSeconds (delay);
+		go.GetComponent<DialogueAnimator> ().Disappear ();
 	}
 }
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -59,9 +59,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_Jumping = false;
             m_AudioSource = GetComponent<AudioSource>();
 			m_MouseLook.Init(transform , m_Camera.transform);
-
-			canLookAround = true;
-			canWalk = true;
         }
 
 
