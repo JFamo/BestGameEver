f7f8cb557c4b931abfa2a9d042415ec8cc07638d
J. Famous <joshua.famous@gmail.com>
2019-01-30 03:03:23 -0500

    Ctesibius dialogue

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -73,8 +73,20 @@ public class Person : MonoBehaviour {
 		ConversationSet c = new ConversationSet();
 
 		if(myID == 1){
-			c.AddConversation( new Conversation("Danny Sanchez", 2, true, "Hey kid. I'm going to say the something.", "Where am I?", "DANNY DONT DO IT DONT SAY IT", 1, -1));
-			c.AddConversation( new Conversation("Danny Sanchez", 1, true, "You're in ancient greece.", "Neat-o", 0));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "Ποιος είσαι? Και γιατί έχετε πάρει το χρόνο να μεταφράσετε!", "Huh?", 1));
+			c.AddConversation( new Conversation("Ctesibius", 2, true, "Sorry, who are you?", "I should be asking you.", "Honestly I'm not really sure.", 2, 3));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "I am Ctesibius! Inventor! Father of Pneumatics!", "Cool! Where is... anyone else?", 5));
+			c.AddConversation( new Conversation("Ctesibius", 2, true, "A question one would ask Plato or Aristotle.", "Do you know them?", "Are they around here somewhere?", 4, 4));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "Oh no, Plato died 200 years ago!", "Ah. Where is everyone else?", 5));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "The rest of the town is still asleep.", "Asleep? It's the middle of the day!", 6));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "Well usually Tromázo wakes everyone up, but he's still asleep.", "So if he doesn't wake up, the town doesn't do anything?", 7));
+			c.AddConversation( new Conversation("Ctesibius", 2, true, "I suppose so. It's rather inefficient... You know, I invented a form of water clock. I wonder if I could use it to herald the arrival of Hellios.", "Like, an alarm clock?", "Who?", 8, 9));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "A... a what? What foreign land do you journey from, stranger? What is this tongue?", "I'm from... some other time. Er, place.", 10));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "Apollo Helios! God of the sun! You're not from around here, are you?", "No, I'm from... some other time. Er, place.", 10));
+			c.AddConversation( new Conversation("Ctesibius", 3, true, "I like the idea of this a-la-rum clock. If only I had some viable way of linking my clock to a loud noise.", "What about a gong?", "What about falling rocks?", "What about a trumpet?", 11, 13, 12));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "Perhaps, but something will need to strike it.", "Well maybe something else then.", 10));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "Ochi, it takes too much air to blow a trumpet.", "Well maybe something else then.", 10));
+			c.AddConversation( new Conversation("Ctesibius", 1, true, "Naí! That's it! Could you get me some pebbles from the pond near town?", "Sure!", -1));
 		}
 
 		return c;
